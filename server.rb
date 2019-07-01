41203cd3e327535db571dd98bfef87176246fa1d
Alex Martell <Alexmartell6@gmail.com>
2019-07-01 10:34:51 -0400

    fixed a formatting issue

--- server.rb
+++ server.rb
@@ -34,7 +34,7 @@ end
 
 oreo = Cake.new("Cookies and Cream Extreme", "Layers of Devil's Food cake with creamy oreo filling inbetween, and Oreo frosting topped with oreos.", "Price: $24.99", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC3srPpkU9nvuBv3XV7ykhEE4j_6CIvOJOAUZrZK4pIuMx59hU")
 hershey = Cake.new("Mean Hershey Machine", "Layers of pound cake with Hershey chocolate syrup inbetween, and vanilla frosting topped with Hershey Kisses.", "Price: $19.99", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm-urOY5wPtaSlXZj6a3mlTTfEmx1uT7DC9O9xTU_RESmMO4xZ")
-cheese = Cake.new("Velvet Dreams", "Layers of red velvet cake with strawberry filling inbetween, and cream cheese frosting topped with glazed strawberries.", "Price: $24.99", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuNLCF_pYdfeSLVvhRVWFQdK7MEzykuf37VGziEo2fYbL6KQDd")
+cheese = Cake.new("Velvet Dreams", "Layers of red velvet cake, and cream cheese frosting topped with glazed strawberries.", "Price: $24.99", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuNLCF_pYdfeSLVvhRVWFQdK7MEzykuf37VGziEo2fYbL6KQDd")
 oatchoc = Cookie.new("Oatmeal Chocolate Chip Cookies","Oatmeal cookies with chocolate chips","Price per cookie: $1.99","Price per dozen: $19.99", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzGgb3eld0arWV2dLzjSjBk98ugZrE6C7tAJAHFh9bfMmOHT80")
 chochip = Cookie.new("Chocolate Chip","Chocolate Chip Cookies","Price per cookie: $1.99","Price per dozen: $19.99", "http://whatsgabycooking.com/wp-content/uploads/GA9A1535.jpg")
 peanut = Cookie.new("Peanut Butter Cookies","Peanut Butter Cookies","Price per cookie: $1.99","Price per dozen: $19.99", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzCtl5TkYKC9UWKAk6Hj-p3lquNC1zfr8v8mGHgEgBRgZGc_lk")
